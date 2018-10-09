7863869ad327758e2fe59cbc0aa5b74921507bf7
Justin <justin.worrall@gmail.com>
2018-10-09 09:03:09 +0100

    added join/2,3

--- src/wol_strings.erl
+++ src/wol_strings.erl
@@ -1,7 +1,9 @@
 -module(wol_strings).
 
 -export([template/2,
-	 template/3]).
+	 template/3,
+	 join/2,
+	 join/3]).
 
 template(Pattern, Args) ->
     template(Pattern, Args, []).
@@ -15,4 +17,20 @@ template(Pattern, Args, none) ->
 template(Pattern, Args, Options) ->
     template(Pattern, Args, proplists:lookup(return, Options)).
     
+join(Names, Delimiter) ->
+    join(Names, Delimiter, []).
+
+join(Names, Delimiter, Options) when is_binary(Delimiter) ->
+    join(Names, binary_to_list(Delimiter), Options);
+join(Names, Delimiter, {_, list}) ->
+    string:join([token(Name) || Name <- Names], Delimiter);
+join(Names, Delimiter, none) ->
+    list_to_binary(string:join([token(Name) || Name <- Names], Delimiter));
+join(Names, Delimiter, Options) ->
+    join(Names, Delimiter, proplists:lookup(return, Options)).
+
+token(Value) when is_binary(Value) ->
+    binary_to_list(Value);
+token(Value) ->
+    Value.
 
