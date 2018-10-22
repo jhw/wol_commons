1de7119a6aaee3c0a6435286426302c06bd77b25
Justin <justin.worrall@gmail.com>
2018-10-22 17:10:17 +0100

    added timedelta function

--- src/wol_datetime.erl
+++ src/wol_datetime.erl
@@ -1,10 +1,14 @@
 -module(wol_datetime).
 
 -export([now_utc/0,
-	 add_seconds/2]).
+	 add_seconds/2,
+	 timedelta/2]).
 
 now_utc() ->
     calendar:now_to_datetime(erlang:timestamp()).
 
 add_seconds(DateTime, Seconds) ->
     calendar:gregorian_seconds_to_datetime(calendar:datetime_to_gregorian_seconds(DateTime)+Seconds).
+
+timedelta(DT0, DT1) ->
+    calendar:datetime_to_gregorian_seconds(DT1)-calendar:datetime_to_gregorian_seconds(DT0).
